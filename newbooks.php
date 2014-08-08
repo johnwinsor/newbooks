ba37c1efccb5ff95d7943d052ecc0feb974347ea
John Winsor <jwinsor@mills.edu>
2014-08-08 16:47:43 -0700

    new template

--- newbooks.php
+++ newbooks.php
@@ -83,27 +83,19 @@ if(isset($_GET["list"])) {
     <link href="/includes/css/global.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
 
-    <!-- scripts -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-    <script>
-        $(function(){
-            $("#millsheader").load("/includes/head.html");
-            $("#millsfooter").load("/includes/foot.html");
-        });
-    </script>
 
 </head>
 <body>
-<div class="container-fluid mainbackground">
-    <div class="row" id="millsheader"></div>
-    <div class="row maincontent">
-		<div class="col-md-12">
+    <div id="millsheader"></div>
+    <div class="maincontent">
+        <div class="container-fluid">
+		    <div class="row" id="millsheader"></div>
 			<div class="row contentcenter">
 				<div class="col-md-12">
 					<?php feed_menu($shelflists); ?>
@@ -115,42 +107,51 @@ if(isset($_GET["list"])) {
 				</div>
 			</div>
 
-<?php
-$items = $p->getItems();     // gets news items
-$covercount = 0;
-foreach (array_slice($items,1) as $i) {
-	$covercount++;
-	//var_dump($i);
-	$desc = string_sanitize($i["book_description"]);
-	$review = $i["user_review"];
-	$coverurl = $i["book_large_image_url"];
-	if (preg_match("/nocover/i", $coverurl)) {
-		$coverurl= "http://www.goodreads.com" . $coverurl;
-	}
-	//print $desc;
-	print "<br />";
-	print "<div class=\"row\">";
-			print "<div class=\"col-md-4\"></div>";
-			print "<div class=\"col-md-4\">";
-					print "<div class=\"covercontainer\" data-toggle=\"popover\" data-container=\"div#caption-" . $covercount . "\" data-title=\"" . $i["title"] . "\" data-content=\"" . $desc . "\"><a href=\"http://library.mills.edu/search/i?SEARCH=" . $i["isbn"] . "&sortdropdown=-&searchscope=6\" target=\"_parent\"><img src=\"" . $coverurl . "\" alt=\"\"></a>";
-							print "<p class=\"booktitle\"><a href=\"http://library.mills.edu/search/i?SEARCH=" . $i["isbn"] . "&sortdropdown=-&searchscope=6\" target=\"_parent\">" . $i["title"] . "</a></span>";
-							print "<p class=\"bookauthor\">by " . $i["author_name"] . "</span>";
-							print "<p class=\"bookauthor\">Call Number: " . $review . "</span>";
-					print "</div>";
-			print "</div>";
-			print "<div class=\"col-md-4\" id=\"caption-" . $covercount . "\"></div>";
-	print "</div>";
-	print "<hr>";
-}
-?>
+			<?php
+			$items = $p->getItems();     // gets news items
+			$covercount = 0;
+			foreach (array_slice($items,1) as $i) {
+				$covercount++;
+				//var_dump($i);
+				$desc = string_sanitize($i["book_description"]);
+				$review = $i["user_review"];
+				$coverurl = $i["book_large_image_url"];
+				if (preg_match("/nocover/i", $coverurl)) {
+					$coverurl= "http://www.goodreads.com" . $coverurl;
+				}
+				//print $desc;
+				print "<br />";
+				print "<div class=\"row\">";
+						print "<div class=\"col-md-4\"></div>";
+						print "<div class=\"col-md-4\">";
+								print "<div class=\"covercontainer\" data-toggle=\"popover\" data-container=\"div#caption-" . $covercount . "\" data-title=\"" . $i["title"] . "\" data-content=\"" . $desc . "\"><a href=\"http://library.mills.edu/search/i?SEARCH=" . $i["isbn"] . "&sortdropdown=-&searchscope=6\" target=\"_parent\"><img src=\"" . $coverurl . "\" alt=\"\"></a>";
+										print "<p class=\"booktitle\"><a href=\"http://library.mills.edu/search/i?SEARCH=" . $i["isbn"] . "&sortdropdown=-&searchscope=6\" target=\"_parent\">" . $i["title"] . "</a></span>";
+										print "<p class=\"bookauthor\">by " . $i["author_name"] . "</span>";
+										print "<p class=\"bookauthor\">Call Number: " . $review . "</span>";
+								print "</div>";
+						print "</div>";
+						print "<div class=\"col-md-4\" id=\"caption-" . $covercount . "\"></div>";
+				print "</div>";
+				print "<hr>";
+			}
+			?>
 		</div>
 	</div> <!-- close maincontent -->
-    <div class="row" id="millsfooter"></div>
-</div> <!-- close container -->
+	<div id="millsfooter"></div>
+
+<!-- scripts -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
 <script src="http://malsup.github.io/jquery.form.js"></script>
+
+<script>
+    $(function(){
+        $("#millsheader").load("/includes/head.html");
+        $("#millsfooter").load("/includes/foot.html");
+    });
+</script>
 
 <script>
 $('[data-toggle="popover"]').popover({
